53a025bdf8e59f4ff18f6ca270446ab9b51bb764
MiguelRocha2001 <migasrocha1@hotmail.com>
2023-04-10 10:48:08 +0100

    Deep Linking (in Web App) is now working!

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {AuthProvider} from "./auth/auth";
 import {BrowserRouter} from "react-router-dom";
 import {StillInProgressAlert} from "./views/StillInProgressAlert";
 import {Col} from "react-bootstrap";
+import {App} from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -19,8 +19,8 @@ root.render(
             <BrowserRouter >
                 <Col style={{width: '90%', margin: 'auto', marginTop: '30px'}}>
                     <StillInProgressAlert />
+                    <App />
                 </Col>
-                <App />
             </BrowserRouter>
         </AuthProvider>
     </React.StrictMode>
@@ -30,3 +30,19 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+function Test() {
+    const [show, setShow] = React.useState<boolean>(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShow(true);
+        }, 1000);
+    }, []);
+
+    if (show) {
+        return <p>Test</p>
+    } else {
+        return <p>Not yet</p>
+    }
+}
