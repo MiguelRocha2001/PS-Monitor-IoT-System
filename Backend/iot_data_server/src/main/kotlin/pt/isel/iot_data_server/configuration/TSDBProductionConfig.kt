7c2fd9c56c91dd5692bdc42e808e8a8f68db114c
psilva20019 <72602379+psilva20019@users.noreply.github.com>
2023-06-11 16:33:31 +0100

    docker file

--- Backend/iot_data_server/src/main/kotlin/pt/isel/iot_data_server/configuration/TSDBProductionConfig.kt
+++ Backend/iot_data_server/src/main/kotlin/pt/isel/iot_data_server/configuration/TSDBProductionConfig.kt
@@ -21,7 +21,8 @@ class TSDBProductionConfig {
 class TSDBBuilder(val bucketName: String) {
     val token: String = System.getenv()["INFLUX_TOKEN"]?:"" // same organization, same token
     val org: String = "isel"
-    val path: String = "http://localhost:8086"
+    val path: String = System.getenv()["INFLUX_URL"]?:"http://localhost:8086" // same organization, same token
+
 
     private val clientThreadLocal = ThreadLocal<InfluxDBClientKotlin>()
 
